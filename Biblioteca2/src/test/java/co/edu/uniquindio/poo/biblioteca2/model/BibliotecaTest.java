8b46fd7a6668eacb45e02ccc9f5a10d6c9b3ffb9
YaelCami <yaelc.calderonv@uqvirtual.edu.co>
2025-05-24 07:13:00 -0500

    Update BibliotecaTest.java

--- Biblioteca2/src/test/java/co/edu/uniquindio/poo/biblioteca2/model/BibliotecaTest.java
+++ Biblioteca2/src/test/java/co/edu/uniquindio/poo/biblioteca2/model/BibliotecaTest.java
@@ -8,10 +8,81 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BibliotecaTest {
     @Test
+    void agregarEmpleado() {
+        Biblioteca biblioteca= new Biblioteca("La Nacional","kenedy");
+        Empleado bibliotecario1= new Bibliotecario("Rafael", "875", "masculino", "rafael@2", "3198", 24, 300.0, Cargo.BIBLIOTECARIO);
+        aasertTrue(biblioteca.agregarEmpleado(bibliotecario1.getIdentificacion()));
+        
+    }
+    @Test
+    void agregarUsuario() {
+        Biblioteca biblioteca= new Biblioteca("Howard","Clementina");
+        Docente docente2=new Docente("Carlos", "524", "masculino","carlos@", "314", 35, tipo.DOCENTE, "547");
+        assertTrue(biblioteca.agregarUsuario(docente2.getIdentificacion());
+    }
+    @Test
+    void agregarLibro() {
+        Biblioteca biblioteca= new Biblioteca("Harvard","La Gran Manzana");
+        LibroFisico libro2= new LibroFisico("Cien años de soledad", "Gabo", "historia", "1980", 4, EstadoLibro.DISPONIBLE, "librosSuperPro", "carrera 8", "350");
+        biblioteca.agregarLibro(libro2.getTitulo());
+        assertFalse(biblioteca.agregarLibro(libro2.getTitulo()));
+        
+    }
+    @Test
+    void buscarLibro() {
+        Biblioteca biblioteca= new Biblioteca("UQ","bARRIO ALTO")
+        LibroDigital libro1= new LibroDigital("Narraciones extraordinarias", "Edgar Allan Poe", "Suspenso", "1800", 5, EstadoLibro.DISPONIBLE,"https.com" );
+        biblioteca.agregarLibro(libro1.getIdentificacion());
+        assertNotNull(libro1, biblioteca.buscarLibro(libro1.getTitulo()));
+    }
+    
+    @Test
     void actualizarUsuario() {
         Biblioteca biblioteca= new Biblioteca("AsombrososLibros","El barrio");
         Estudiante estudiante2= new Estudiante("Estefanny","458","femenina", "@estef","314269", 19, Tipo.ESTUDIANTE, "849");
         assertFalse(biblioteca.actualizarUsuario(estudiante2.getIdentificacion(), estudiante2));
+    }
+    @Test
+    void buscarUsuario() {
+        Biblioteca biblioteca= new Biblioteca("UQ","bARRIO ALTO,");
+        Estudiante estudiante1= new Estudiante("jUAN","123","nobinario", "@wre","312",12, Tipo.ESTUDIANTE, "123");
+        biblioteca.agregarUsuario(estudiante1.getIdentificacion());
+        assertEquals(estudiante1,biblioteca.buscarUsuario(estudiante1.getIdentificacion()));
+    }
+
+    @Test
+    void buscarEmpleado() {
+        Biblioteca biblioteca= new Biblioteca("universidad de los andes","Laureles");
+        Empleado bibliotecario4= new Bibliotecario("Alejo", "167", "masculino", "alejo@2", "3125", 18, 2.5, Cargo.BIBLIOTECARIO);
+        biblioteca.agregarEmpleado(biblitecario4.getIdentificacion());
+        assertNotNull(bibliotecario4, biblioteca.buscarEmpleado(bibliotecario4.getIdentificacion()));
+    }
+
+    @Test
+    void buscarPrestamo() {
+        Biblioteca biblioteca= new Biblioteca("La Gran Colombia","Rocio");
+        Estudiante estudiante1= new Estudiante("jUAN","123","nobinario", "@wre","312",12, Tipo.ESTUDIANTE, "123");
+        LibroDigital libro1= new LibroDigital("Narraciones extraordinarias", "Edgar Allan Poe", "Suspenso", "1800", 5, EstadoLibro.DISPONIBLE,"https.com" );
+        biblioteca.agregarUsuario(estudiante1.getIdentificacion());
+        biblioteca.agregarLibro(libro1.getIdentificacion());
+        Prestamo prestamo1= new Prestamo(22/05/2025,02/06/2025,28/05/2025,0.0, True, 123, libro1, estudiante1);
+        biblioteca.agregarPrestamo(prestamo1.getId());
+        assertEquals(prestamo1, biblioteca.buscarPrestamo(prestamo1.getId()));
+    }
+    @Test
+    void eliminarLibro() {
+        Biblioteca biblioteca= new Biblioteca("Jorge Isaacs","Quimbaya");
+        LibroDigital libro3= new LibroDigital("Hamlet", "William Shakespaeare", "Drama", "1560", 10, EstadoLibro.DISPONIBLE,"https.hamlet.com");
+        biblioteca.agregarLibro(libro3.getTitulo()));
+        assertTrue(biblioteca.eliminarLibro(libro3.getTitulo()));
+    }
+
+    @Test
+    void eliminarEmpleado() {
+        Biblioteca biblioteca= new Biblioteca("Albert Einstien","Coointevi");
+        Bibliotecario bibliotecario3= new Bibliotecario("Sara", "157", "femenino", "sara@2", "95400", 50, 500.0, Cargo.BIBLIOTECARIO);
+        biblioteca.agregarEmpleado(bibliotecario4.getIdentificacion());
+        assertTrue(biblioteca.eliminarEmpleado(bibliotecario3.getIdentificacion()));
     }
 
     @Test
