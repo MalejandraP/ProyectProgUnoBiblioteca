13943de7165e573d051c4ecc626205e16929ce96
YaelCami <yaelc.calderonv@uqvirtual.edu.co>
2025-05-24 20:09:29 -0500

    Update BibliotecarioTest.java

--- Biblioteca2/src/test/java/co/edu/uniquindio/poo/biblioteca2/model/BibliotecarioTest.java
+++ Biblioteca2/src/test/java/co/edu/uniquindio/poo/biblioteca2/model/BibliotecarioTest.java
@@ -10,7 +10,8 @@ class BibliotecarioTest {
         Biblioteca biblioteca= new Biblioteca("Rafael Pombo", "Los pinos");
         LibroDigital libro7= new LibroDigital("La Odisea", "Homero", "Épico", "400", false, EstadoLibro.DISPONIBLE, "https.homero.com");
         LibroFisico libro8= new LibroFisico("Orgullo y prejuicio", "Romance", "1989", true, EstadoLibro.PRESTADO, "Los tres editores", "2548", 350);
-
+        biblioteca.agregarLibro(libro7);
+        biblioteca.agregarLibro(libro8);
     }
 
     @Test
