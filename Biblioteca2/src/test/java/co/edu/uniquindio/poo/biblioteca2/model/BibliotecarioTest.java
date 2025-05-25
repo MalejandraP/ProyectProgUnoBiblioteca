a8d8b88f91c77901b27abc074fe2f1cda61d1199
YaelCami <yaelc.calderonv@uqvirtual.edu.co>
2025-05-24 22:27:45 -0500

    Update BibliotecarioTest.java

--- Biblioteca2/src/test/java/co/edu/uniquindio/poo/biblioteca2/model/BibliotecarioTest.java
+++ Biblioteca2/src/test/java/co/edu/uniquindio/poo/biblioteca2/model/BibliotecarioTest.java
@@ -75,6 +75,8 @@ class BibliotecarioTest {
         biblioteca.agregarLibro(libro11);
         Prestamo prestamo1= new Prestamo(LocalDate.of(2025,2,8), "96375", libro9, estudiante1);
         Prestamo prestamo2= new Prestamo(LocalDate.of(2025,5,4), "9025", libro10, docente1);
+        biblioteca.agregarPrestamo(prestamo1);
+        biblioteca.agregarPrestamo(prestamo2);
         List<Libro> listEsperados= List.of(libro9, libro10);
         assertIterableEquals(listEsperados, biblioteca.reporteLibrosPrestados());
         
@@ -82,6 +84,21 @@ class BibliotecarioTest {
 
     @Test
     void calculaDeudaTotal() {
+        Biblioteca biblioteca= new Biblioteca("Le petit prince", "Francia");
+        LibroDigital libro10= new LibroDigital("Narraciones extraordinarias", "Edgar Allan Poe", "Suspenso", "1800", false, EstadoLibro.DISPONIBLE,"https.com" );
+        LibroFisico libro11 = new LibroFisico("Rey Lear", "William Shakeapeare", "drama", "1580",true, EstadoLibro.DISPONIBLE, "librosSuperPro", "carrera 4", 120);
+        Docente docente4= new Docente("Cayetano", "7540","masculino", "cayetano@","32037",59, tipo.DOCENTE,"381");
+        biblioteca.agregarLibro(libro10);
+        biblioteca.agregarLibro(libro11);
+        bibiloteca.agregarUsuario(docente4);
+        Prestamo prestamo1 = new Prestamo(LocalDate.of(2025,4,9), "02149", libro10, docente4);
+        Prestamo prestamo2 = new Prestamo(LocalDate.of(2025,4,11), "03349", libro11, docente4);
+        biblioteca.agregarPrestamo(prestamo1);
+        biblioteca.agregarPrestamo(prestamo2);
+        
+        assertEquals(14000.0, biblioteca.calcularDeudaTotal("7540", LocalDate(2025,5,3)) );
+        
+        
     }
 
     @Test
